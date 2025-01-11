b4223389c7f180c862c728e38e7f003b24098e8b
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2025-01-11 15:52:48 +0200

    Added error handling for login event

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,33 +4,39 @@ import { getKeyVaultSecret } from '../handlers/credentialHandler';
 
 const login = async (page: Page): Promise<Page> => {
     LoggerHandler.log('Starting login process.');
-
-    const email: string = process.env.NODE_ENV === 'production' ? await getKeyVaultSecret('user-email') : process.env.EMAIL;
-    const password: string = process.env.NODE_ENV === 'production' ? await getKeyVaultSecret('user-password') : process.env.PASSWORD;
     
-    await page.goto(process.env.TARGET_URL);
-    
-    const emailInput = await page.waitForSelector('input[name="loginfmt"]');
-    await emailInput.fill(email);
+    try {
+        const email: string = process.env.NODE_ENV === 'production' ? await getKeyVaultSecret('user-email') : process.env.EMAIL;
+        const password: string = process.env.NODE_ENV === 'production' ? await getKeyVaultSecret('user-password') : process.env.PASSWORD;
+        
+        await page.goto(process.env.TARGET_URL);
+        
+        const emailInput = await page.waitForSelector('input[name="loginfmt"]');
+        await emailInput.fill(email);
 
-    const nextButton = await page.waitForSelector('text="Next"');
-    await nextButton.click();
+        const nextButton = await page.waitForSelector('text="Next"');
+        await nextButton.click();
 
-    const passwordInput = await page.waitForSelector('input[id="passwordInput"]');
-    await passwordInput.fill(password);
+        const passwordInput = await page.waitForSelector('input[id="passwordInput"]');
+        await passwordInput.fill(password);
 
-    const signInButton = await page.waitForSelector('text="Sign in"');
-    await signInButton.click();
+        const signInButton = await page.waitForSelector('text="Sign in"');
+        await signInButton.click();
 
-    const isMyDeviceButton = await page.waitForSelector('text="Yes, this is my device"');
-    await isMyDeviceButton.click();
+        const isMyDeviceButton = await page.waitForSelector('text="Yes, this is my device"');
+        await isMyDeviceButton.click();
 
-    const staySignedInButton = await page.waitForSelector('text="Yes"');
-    await staySignedInButton.click();
+        const staySignedInButton = await page.waitForSelector('text="Yes"');
+        await staySignedInButton.click();
 
-    LoggerHandler.log('Login successful.');
+        LoggerHandler.log('Login successful.');
 
-    return page;
+        return page;
+    } catch (error: any) {
+        const msg: string = `Error occurred while logging in: ${error}`;
+        LoggerHandler.error(msg);
+        throw new Error(msg);   
+    }
 }
 
 export { login };
